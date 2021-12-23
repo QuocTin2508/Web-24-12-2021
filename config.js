0819e2e98b4dd3720e2d27f1dff680c1648e1ba7
QuocTin2508 <vqt5425@gmail.com>
2021-12-23 11:05:25 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Bích Vân';
+const giftUrl = 'I Love You.html';
+const eventName = 'Merry Christmas';
+const titleCard = 'Người mãi trong trái tim tôi';
+const contentCard = 'Giáng sinh an lành, mọi điều may mắn sẽ đến với em, hãy luôn nở nụ cười nhé, anh sẽ luôn là người ủng hộ em';
+
+
+const giftImage = '139790278_228572875631615_7114661327778356776_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
